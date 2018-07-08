01509a9188acc51e907083dcd73eb868c94023a4
VjacheslavZ <slavikov.net@mail.ru>
2018-07-08 17:01:08 +0300

    add components: News Main Post Videos

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home/home';
+import NewsMain from './components/Article/News/Main/index';
 import Layout from './hoc/Layout/layout';
-import NewsArticles from "./components/Article/News/Post/index";
+import NewsArticles from './components/Article/News/Post/index';
+import VideoArticle from './components/Article/Videos/Video/index';
+import VideosMain from './components/Article/Videos/Main/index';
 
 class Routes extends Component {
     render(){
@@ -11,7 +14,10 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home}/>
+                    <Route path="/news" exact component={NewsMain}/>
                     <Route path="/articles/:id" exact component={NewsArticles}/>
+                    <Route path="/videos/:id" exact component={VideoArticle}/>
+                    <Route path="/videos" exact component={VideosMain}/>
                 </Switch>
             </Layout>
         )
