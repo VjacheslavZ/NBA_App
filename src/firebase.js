d09219939557c77a71017a48fc5ee61a0294675c
VjacheslavZ <slavikov.net@mail.ru>
2018-07-10 00:37:54 +0300

    firebase info

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyD5HO2c4wzYgAU7rUCiXUqc9cWubIhuZo4",
-    authDomain: "nba-app-22676.firebaseapp.com",
-    databaseURL: "https://nba-app-22676.firebaseio.com",
-    projectId: "nba-app-22676",
-    storageBucket: "nba-app-22676.appspot.com",
-    messagingSenderId: "135128998903"
+
 };
 
 firebase.initializeApp(config);
